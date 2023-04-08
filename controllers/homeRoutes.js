9e1a525c524f5b307800458099fc155aa8063882
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-04-08 15:56:37 -0400

    added signup route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,22 +4,22 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all post and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // // Get all post and JOIN with user data
+    // const postData = await Post.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
 
     // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
+    // const posts = postData.map((post) => post.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage',  
-      {posts} 
+      // {posts} 
     );
   } catch (err) {
     res.status(500).json(err);
--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const dashboardRoutes = require('./dashboardRoute');
 
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+// router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
