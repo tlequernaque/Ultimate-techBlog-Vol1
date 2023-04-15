f9145530e92d58d5af9c93c56e170f81fe27fe35
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-04-15 07:38:23 -0400

    added homehandlebars

---
+++ public/js/home.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#post-name').value.trim();
+    const description = document.querySelector('#post-content').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('unable to create post');
+      }
+    }
+  };
+  
+  // const delButtonHandler = async (event) => {
+  //   if (event.target.hasAttribute('data-id')) {
+  //     const id = event.target.getAttribute('data-id');
+  
+  //     const response = await fetch(`/api/projects/${id}`, {
+  //       method: 'DELETE',
+  //     });
+  
+  //     if (response.ok) {
+  //       document.location.replace('/profile');
+  //     } else {
+  //       alert('Failed to delete project');
+  //     }
+  //   }
+  // };
+  
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  // document
+  //   .querySelector('.project-list')
+  //   .addEventListener('click', delButtonHandler);
+  
