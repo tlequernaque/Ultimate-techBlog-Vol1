c6eeffacbc17be864ecfedf796b141fab90de933
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-04-15 22:07:53 -0400

    got dashboard to remder

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { Post, Comment , User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/dashboard', async(req, res) => {
+router.get('/', async(req, res) => {
 
     try{
   
